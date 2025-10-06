a7f302e083eadf79d3f60146698a4d73d16f2465
rzhbadhon <rhbadhon030@gmail.com>
2025-10-06 22:56:10 +0600

    Cors fixed

--- handlers/cors.go
+++ handlers/cors.go
@@ -9,8 +9,12 @@ import (
 func Cors(next http.Handler) http.Handler {
 
 	c := cors.New(cors.Options{
-
-		AllowedOrigins: []string{"https://rzhbadhon.github.io"},
+		
+		 AllowedOrigins: []string{
+        "https://go-jwt-tool.github.io", 
+        "https://rzhbadhon.github.io",  
+        "null",                        
+    },
 
 		AllowedMethods: []string{"POST", "GET", "OPTIONS"},
 
